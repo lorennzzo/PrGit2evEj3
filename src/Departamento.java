d4aecc2ef334317abd44baabc2b833c947840f67
lorennzzo <alvaro.lorenzo@alumno.iesluisvives.org>
2025-03-19 14:08:30 +0100

    apartado 1

---
+++ src/Departamento.java
@@ -0,0 +1,8 @@
+public class Departamento {
+    private String nombre;
+    private int numEmpleados;
+    private Empleado[] empleados = new Empleado[2];
+    void anadir(Empleado v){
+        empleados[0]=v;
+    }
+}
---
+++ src/Empleado.java
@@ -0,0 +1,6 @@
+public class Empleado {
+    private String nombre;
+    private int edad;
+    private double sueldo;
+
+}
